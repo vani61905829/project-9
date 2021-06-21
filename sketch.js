ea2cab1f0566238d6f37073abceaa07d80b50580
vani61905829 <86050983+vani61905829@users.noreply.github.com>
2021-06-21 23:18:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+}
+
+function draw() {
+  background(r,g,btn_red);
+}
+
+In function setup():
+Create buttons for Red and Green colors.
+mousePressed(;)
